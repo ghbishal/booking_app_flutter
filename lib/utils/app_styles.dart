1824217319fd2fddce6f1604b1c3f3c8c63339d6
ghbishal <bishalk2056@gmail.com>
2024-02-02 18:12:27 +0900

    fix: color

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -6,7 +6,7 @@ class Styles {
   static Color primaryColor = primary;
   static const Color textColor = Color(0xFF3b3b3b);
   static const Color bgColor = Color(0xFFeeedf2);
-  static const Color orangeColor = Color(0xFF526799);
+  static const Color orangeColor = Color(0xFFF37B67);
 
   static TextStyle textStyle = const TextStyle(
       fontSize: 16, color: textColor, fontWeight: FontWeight.w500);
